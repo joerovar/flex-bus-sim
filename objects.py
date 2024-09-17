ef0fc89f460cc4bd083cfdb81d19f0e612202aa0
Joseph Rodriguez <88448128+joerovar@users.noreply.github.com>
2024-09-17 16:14:50 -0400

    new version

--- objects.py
+++ objects.py
@@ -1,23 +1,27 @@
 class Stop:
-    def __init__(self, id, name, stop_type, demand):
-        self.id = id
-        self.stop_type = stop_type
-        self.demand = demand
+    def __init__(self, flex=False):
+        self.pax = 0
+        self.flex = flex
+
+    def generate_pax(t):
+        return
+
 
 class Route:
-    def __init__(self, id, stops, link_times):
-        self.id = id
-        self.stops = stops
-        self.link_times = link_times
+    def __init__(self):
+        n_stops = 7
+        flex_stops = [3, 5]
+        self.stops = {
+            'in': [Stop(i in flex_stops) for i in range(n_stops)],
+            'out': [Stop(i in flex_stops) for i in range(n_stops)]
+        }
 
+        demand = {
+            'out': 75,
+            'in': 60
+        }
+        
         ## in case they are flex segments
-
-class Passenger:
-    def __init__(self, id, source_stop, destination_stop, arrival_time):
-        self.id = id
-        self.source_stop = source_stop
-        self.destination_stop = destination_stop
-        self.arrival_time = arrival_time
 
 class Vehicle:
     def __init__(self, id, route, capacity):
@@ -26,8 +30,24 @@ class Vehicle:
         self.capacity = capacity
         self.location = route.sequence[0]
 
-class Driver:
-    def __init__(self, id, vehicle):
-        self.id = id
-        self.vehicle = vehicle
-        self.status = 'idle'
+class EventManager:
+    def __init__(self) -> None:
+        
+    
+
+
+# class Passenger:
+#     def __init__(self, id, source_stop, destination_stop, arrival_time):
+#         self.id = id
+#         self.source_stop = source_stop
+#         self.destination_stop = destination_stop
+#         self.arrival_time = arrival_time
+
+
+
+# class Driver:
+#     def __init__(self, id, vehicle):
+#         self.id = id
+#         self.vehicle = vehicle
+#         self.status = 'idle'
+
